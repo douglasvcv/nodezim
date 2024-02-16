288220f1bbe5a9bf5f29f6aefb0197c170bfc414
Douglas <douglasvitor1233@gmail.com>
2024-02-16 18:38:49 -0300

    add practice with params

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.get("/novo", (req, res) =>{
     res.send("Bem-vindo")
 })
 
-
+app.get("/ola/:nome/:cargo", (req, res) => {
+    res.send(`<h1>Olá ${req.params.nome}, você trabalha como ${req.params.cargo}</h1>`)
+})
 
 app.listen(3081, () => console.log("Servidor rodando na url http://localhost:3081"))
 
