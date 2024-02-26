fd6d1949f76d2762d250a270765d6fb522c6c8a5
Douglas <douglasvitor1233@gmail.com>
2024-02-26 15:32:48 -0300

    installing and configuring handlerbars

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
+require('dotenv').config()
+
 const express = require("express")
 const app = express()
+const { engine } = require('express-handlebars')
+const Sequelize = require('sequelize')
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/html/index.html")
-})
-app.get("/novo", (req, res) =>{
-    res.send("Bem-vindo")
-})
 
-app.get("/ola/:nome/:cargo", (req, res) => {
-    res.send(`<h1>Olá ${req.params.nome}, você trabalha como ${req.params.cargo}</h1>`)
-})
+
+//Config
+    //Template Engine
+    app.engine('handlebars', engine({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+
+
+    //Connection with MySQL database
+    const sequelize = new Sequelize('testenovo', 'root', process.env.MYSQLPASSWORD, {
+        host: 'localhost',
+        dialect: 'mysql'
+    })
+
+    //Routes
+    app.get('/bak', (req, res) =>{
+        res.send("han han cheguei")
+    })
+
+
+// app.get("/", (req, res) => {
+//     res.sendFile(__dirname + "/html/index.html")
+// })
+// app.get("/novo", (req, res) =>{
+//     res.send("Bem-vindo")
+// })
+
+// app.get("/ola/:nome/:cargo", (req, res) => {
+//     res.send(`<h1>Olá ${req.params.nome}, você trabalha como ${req.params.cargo}</h1>`)
+// })
 
 app.listen(3081, () => console.log("Servidor rodando na url http://localhost:3081"))
 
--- teste.js
+++ teste.js
@@ -1,9 +1,46 @@
-require('dotenv').config()
+// require('dotenv').config();
 
- const Sequelize = require('sequelize')
- const sequelize = new Sequelize('testenovo', 'root', process.env.MYSQLPASSWORD, {
-    host: "localhost",
-    dialect: 'mysql'
- } )
+//  const Sequelize = require('sequelize')
+//  const sequelize = new Sequelize('testenovo', 'root', process.env.MYSQLPASSWORD, {
+//     host: "localhost",
+//     dialect: 'mysql'
+//  } );
 
-sequelize.authenticate().then(() => console.log("funcionou")).catch((error) => console.log("não funcionou: "+error))
+// sequelize.authenticate()
+// .then(() => console.log("funcionou"))
+// .catch((error) => console.log("não funcionou: "+error));
+
+// const Postagem = sequelize.define('postagens', {
+//     Titulo: {
+//         type: Sequelize.STRING
+//     },
+//     Conteudo: {
+//         type: Sequelize.TEXT
+//     }
+// })
+
+// // Postagem.sync({force:true})
+// // Postagem.create({
+// //     Titulo: "Tomale tomale",
+// //     Conteudo: "Vai tomando ta so tomaleadasdadasd"
+// // })
+
+// const Usuario = sequelize.define('usuarios', {
+//     nome: {
+//         type: Sequelize.STRING
+//     },
+//     idade: {
+//         type: Sequelize.INTEGER
+//     },
+//     email: {
+//         type: Sequelize.STRING
+//     }
+// })
+
+// // Usuario.sync({force:true})
+
+// Usuario.create({
+//     nome: "Monaco",
+//     idade: 29,
+//     email:"monacofk@gmail.com"
+// })
